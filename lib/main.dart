fa9f8ca4d60ea345826e11fa00f4846838cabe2c
iamageo <geovani.af4@gmail.com>
2023-06-03 19:19:14 -0300

    disable debug banner and navigate to example screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mvvm_template/ui/example_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter MVVM Template',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: const ExampleScreen(),
     );
   }
 }
