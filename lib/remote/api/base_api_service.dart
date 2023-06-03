82b02ed8a58e930e4674f0fbc0148175f13b49ca
iamageo <geovani.af4@gmail.com>
2023-06-03 19:22:50 -0300

    creating repository and repository impl

---
+++ lib/remote/api/base_api_service.dart
@@ -0,0 +1,8 @@
+abstract class BaseApiService {
+
+  final String baseUrl = "https://jsonplaceholder.typicode.com/";
+
+  Future<dynamic> getResponse(String url);
+  Future<dynamic> postResponse(String url,Map<String, String> jsonBody);
+
+}
---
+++ lib/remote/repository/app_repository_impl.dart
@@ -0,0 +1,28 @@
+import '../api/api_endpoints.dart';
+import '../api/api_service.dart';
+import '../api/base_api_service.dart';
+import '../model/base_model.dart';
+import 'app_repository.dart';
+
+class AppRepositoryImpl implements AppRepository {
+
+  final BaseApiService _apiService = NetworkApiService();
+
+  @override
+  Future<List<BaseModel>> getDataExample() async {
+    try {
+      print("cheguei na rota");
+      dynamic response = await _apiService.getResponse(ApiEndPoints().getData);
+
+      if (response is List) {
+        List<BaseModel> jsonData = response.map((item) => BaseModel.fromJson(item)).toList();
+        return jsonData;
+      } else {
+        throw FormatException('Received data is not a list');
+      }
+
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
